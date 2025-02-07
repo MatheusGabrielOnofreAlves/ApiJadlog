6649fc4efc96c6e361b4a67caa72178072a3a590
Matheus Gabriel Onofre Alves <112907058+MatheusGabrielOnofreAlves@users.noreply.github.com>
2025-02-07 05:48:10 -0300

    Update Cliente.cs

--- MeuProjeto/Models/Cliente.cs
+++ MeuProjeto/Models/Cliente.cs
@@ -4,10 +4,8 @@
     {
         public string CPF { get; set; }
         public string Nome { get; set; }
-        public string CEP { get; set; }
         public string Endereco { get; set; }
         public string Produto { get; set; }
-        public DateTime? DataCompra { get; set; } 
 
     }
 }
